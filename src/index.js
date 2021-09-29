ada679366062374b58ba87d9b5695b9f5bfac97c
mahbha <43632182+mahbha@users.noreply.github.com>
2021-09-29 21:19:44 +0530

    React components

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-//var today = new Date();
-
-const customStyle = {
-  color: "red"
-};
-
-var today = new Date(2021, 9, 29, 19, 0, 0);
-var time = today.getHours();
-var greeting = "";
-
-if (time < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "red";
-} else if ((time) => 12 && time < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "green";
-} else {
-  greeting = "Good Evening";
-  customStyle.color = "blue";
-}
-
-ReactDOM.render(
-  <div>
-    <h1 className="heading" style={customStyle}>
-      {greeting}
-    </h1>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
